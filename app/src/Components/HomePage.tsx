08ebfe78a53697d4ae96ca4477d13db86a22d79e
Eric Soros <ericsoros@Erics-MacBook-Pro-2.local>
2023-04-04 21:05:51 -0400

    fixing ui a bit

--- app/src/Components/HomePage.tsx
+++ app/src/Components/HomePage.tsx
@@ -19,18 +19,20 @@ export default function HomePage(props: {
         let mintArgs = {
             amount: amount,
             duration: duration,
-            unit: unit
+            unit: unit,
+            address: token.address
         }
         alert("call smart contract here..." + JSON.stringify(mintArgs))
     }
 
-    return <div style={{display: "flex", flexDirection: "column"}}>
-        <div style={{display: "flex", flexDirection: "column", border: "solid black .1rem", height: "10rem", width: "30rem", alignItems: "center", justifyContent: "center", borderRadius: ".25rem"}}>
+    return <div style={{display: "flex", flexDirection: "column", width: "100%", height: "100%"}}>
+        <div style={{flexGrow: 1}} />
+        <div style={{placeSelf: "center", display: "flex", flexDirection: "column", height: "30%", width: "60%", alignItems: "center", justifyContent: "center", borderRadius: ".25rem", backgroundColor: "rgb(249,249,249)"}}>
             <div style={{display: "flex", flexDirection: "row"}}>
                 <input type="range" value={duration} min={1} max={100} onChange={(e) => setDuration(parseInt
                     (e.target.value))} />
                 <p>{duration}</p>
-                <select value={unit} onChange={(e) => setUnit(e.target.value)} style={{margin: 0, padding: 0}}>
+                <select value={unit} onChange={(e) => setUnit(e.target.value)} style={{height: "4vh", placeSelf: "center"}}>
                     <option value={"seconds"}>seconds</option>
                     <option value={"minutes"}>minutes</option>
                     <option value={"hours"}>hours</option>
@@ -38,13 +40,14 @@ export default function HomePage(props: {
                     <option value={"years"}>years</option>
                 </select>
             </div>
-            <div style={{display: "flex", flexDirection: "row"}}>
+            <div style={{display: "flex", flexDirection: "row", margin: "1.5%", gap: "3%"}}>
                 <input value={amount} onChange={(e) => setAmount(e.target.value)} />
                 <img onClick={() => alert("show change token modal here...")} style={{height: "1.5rem"}} src={`/${token.name}.svg`} />
             </div>
-            <button onClick={() => mint()} style={{width: "90%"}}>
+            <button onClick={() => mint()} style={{backgroundColor: "rgb(122,74,221)", width: "70%", margin: "1.5%"}}>
                 Mint
             </button>
         </div>
+        <div style={{flexGrow: 1}} />
     </div>
 }
